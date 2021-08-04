ed210b43ab75490f64d2559ae2d96d882a37e3b4
liuzhaoluliuzhaolu <liuzhaoluliuzhaolu@didiglobal.com>
2021-08-04 20:35:58 +0800

    handler类

---
+++ src/main/java/com/zero/ispring/servlet/Handler.java
@@ -0,0 +1,58 @@
+package com.zero.ispring.servlet;
+
+import com.zero.ispring.annotations.iRequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.regex.Pattern;
+
+/**
+ * @author liuzhaoluliuzhaolu
+ * @date 2021/8/4 8:00 下午
+ * @desc
+ * @prd
+ * @Modification History:
+ * Date         Author          Description
+ * ------------------------------------------ *
+ */
+public class Handler {
+    private Object controller;
+    private Method method;
+    private Pattern pattern;
+    private LinkedList<String> paramList;
+
+    public Handler(Object controller, Method method, Pattern pattern) {
+        this.controller = controller;
+        this.method = method;
+        this.pattern = pattern;
+        paramList = new LinkedList<>();
+        putParamsIndex(method);
+    }
+
+    private void putParamsIndex(Method method){
+        // 提取加了iRequestParam的参数
+        Annotation[][] annotations = method.getParameterAnnotations();
+        Arrays.stream(annotations).forEach(annotations1 -> {
+            Arrays.stream(annotations1).forEach(annotation -> {
+                if(annotation instanceof iRequestParam){
+                    String name = ((iRequestParam) annotation).value();
+                    if(!"".equals(name.trim())){
+                        paramList.add(name);
+                    }
+                }
+            });
+        });
+
+        // 提取req & rsp
+        Class<?>[] paramTypes = method.getParameterTypes();
+        Arrays.stream(paramTypes).forEach(type -> {
+            if(type == HttpServletRequest.class || type == HttpServletResponse.class){
+                paramList.add(type.getName());
+            }
+        });
+
+    }
+}
